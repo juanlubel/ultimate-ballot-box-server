b8c5e1d8f57ff3a50e89597082b5de80fc8a24d5
juanlubel <juanluis.belda@gmail.com>
2019-08-29 18:14:34 +0200

    app.js working

---
+++ app.js
@@ -0,0 +1,21 @@
+const express = require("express"),
+    app = express(),
+    bodyParser  = require("body-parser"),
+    methodOverride = require("method-override");
+mongoose = require('mongoose');
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(methodOverride());
+
+const router = express.Router();
+
+router.get('/', function(req, res) {
+    res.send("Hello World!");
+});
+
+app.use(router);
+
+app.listen(3000, function() {
+    console.log("Node server running on http://localhost:3000");
+});
